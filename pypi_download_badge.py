249c31984ffb2b375c65d538ae2e512f2ffd1dac
ganler <jaway.liu@gmail.com>
2023-10-28 22:11:57 -0500

    feat: compile pypi badge everyday

---
+++ pypi_download_badge.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    import requests
+
+    projects = ["evalplus", "nnsmith"]
+    for project in projects:
+        url = f"https://pypistats.org/api/packages/{project}/overall"
+        response = requests.get(url)
+        data = response.json()["data"]
+        total_downloads = sum([x["downloads"] for x in data])
+        print(f"{project}: {total_downloads}")
+        badge_url = f"https://img.shields.io/badge/pip_install-{total_downloads}-white?style=social&logo=pypi&logoColor=b509ac"
+        # compile it to an svg
+        response = requests.get(badge_url)
+        with open(f"{project}_pypi.svg", "wb") as f:
+            f.write(response.content)
